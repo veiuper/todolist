ebafcc0afad32caa1881f04d2611abcc6a08685b
veiuper <valeriyfinans@gmail.com>
2022-03-29 14:47:56 +0500

    + TaskController.java

---
+++ src/main/java/com/veiuper/todolist/controller/TaskController.java
@@ -0,0 +1,41 @@
+package com.veiuper.todolist.controller;
+
+import com.veiuper.todolist.model.Task;
+import com.veiuper.todolist.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping("/")
+    public String getAll(Model model) {
+        List<Task> taskList = taskService.getAll();
+        model.addAttribute("taskList", taskList);
+        model.addAttribute("taskSize", taskList.size());
+        return "index";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String deleteTask(@PathVariable int id) {
+        taskService.delete(id);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/delete")
+    public String deleteAll() {
+        taskService.deleteAll();
+        return "redirect:/";
+    }
+
+    @PostMapping("/add")
+    public String addTask(@ModelAttribute Task task) {
+        taskService.save(task);
+        return "redirect:/";
+    }
+}
