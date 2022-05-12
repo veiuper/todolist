23d96860dfc5f9e4044aa884502dea55b18a4995
veiuper <valeriyfinans@gmail.com>
2022-05-13 00:19:55 +0500

    Change ConfirmationTokenRepository.java

--- src/main/java/com/veiuper/todolist/dao/ConfirmationTokenRepository.java
+++ src/main/java/com/veiuper/todolist/dao/ConfirmationTokenRepository.java
@@ -4,6 +4,9 @@ import com.veiuper.todolist.model.ConfirmationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Long> {
+    Optional<ConfirmationToken> findConfirmationTokenByConfirmationToken(String token);
 }
