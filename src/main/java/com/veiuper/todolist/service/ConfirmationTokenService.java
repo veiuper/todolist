dc00f61a684b954b8b92e2c552de144feeb53ea5
veiuper <valeriyfinans@gmail.com>
2022-05-12 22:04:06 +0500

    Add ConfirmationTokenService.java

---
+++ src/main/java/com/veiuper/todolist/service/ConfirmationTokenService.java
@@ -0,0 +1,16 @@
+package com.veiuper.todolist.service;
+
+import com.veiuper.todolist.dao.ConfirmationTokenRepository;
+import com.veiuper.todolist.model.ConfirmationToken;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ConfirmationTokenService {
+    private final ConfirmationTokenRepository confirmationTokenRepository;
+
+    public void save(ConfirmationToken confirmationToken) {
+        confirmationTokenRepository.save(confirmationToken);
+    }
+}
