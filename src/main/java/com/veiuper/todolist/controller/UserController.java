3c72c183a168504e4afcd2ec06344f4b24f7e432
veiuper <valeriyfinans@gmail.com>
2022-05-13 00:23:33 +0500

    Add UserController.java

---
+++ src/main/java/com/veiuper/todolist/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.veiuper.todolist.controller;
+
+import com.veiuper.todolist.model.ConfirmationToken;
+import com.veiuper.todolist.model.User;
+import com.veiuper.todolist.service.ConfirmationTokenService;
+import com.veiuper.todolist.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
+@Controller
+@AllArgsConstructor
+public class UserController {
+    private final UserService userService;
+    private final ConfirmationTokenService confirmationTokenService;
+
+    @GetMapping("/sign-in")
+    public String signIn() {
+        return "sign-in";
+    }
+
+    @GetMapping("/sign-up")
+    public String signUp() {
+        return "sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUp(User user) {
+        userService.signUpUser(user);
+        return "redirect:/sign-in";
+    }
+
+    @GetMapping("/confirm")
+    String confirmMail(@RequestParam("token") String token) {
+        Optional<ConfirmationToken> optionalConfirmationToken = confirmationTokenService.findConfirmationTokenByConfirmationToken(token);
+        optionalConfirmationToken.ifPresent(userService::confirmUser);
+        return "/sign-in";
+    }
+}
