8e0fa63f5a3eece08d0316d2894229e513add554
veiuper <valeriyfinans@gmail.com>
2022-05-12 22:24:00 +0500

    Add signUpUser method for UserService.java

--- src/main/java/com/veiuper/todolist/service/UserService.java
+++ src/main/java/com/veiuper/todolist/service/UserService.java
@@ -1,11 +1,13 @@
 package com.veiuper.todolist.service;
 
 import com.veiuper.todolist.dao.UserRepository;
+import com.veiuper.todolist.model.ConfirmationToken;
 import com.veiuper.todolist.model.User;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.text.MessageFormat;
@@ -15,6 +17,9 @@ import java.util.Optional;
 @AllArgsConstructor
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
+    private final ConfirmationTokenService confirmationTokenService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         final Optional<User> optionalUser = userRepository.findByEmail(email);
@@ -22,5 +27,13 @@ public class UserService implements UserDetailsService {
             throw new UsernameNotFoundException(MessageFormat.format("Пользователь с email {0} не найден.", email));
         }
         return optionalUser.get();
+    }
+
+    public void signUpUser(User user) {
+        final String encryptedPassword = bCryptPasswordEncoder.encode(user.getPassword());
+        user.setPassword(encryptedPassword);
+        final User createdUser = userRepository.save(user);
+        final ConfirmationToken confirmationToken = new ConfirmationToken(user);
+        confirmationTokenService.save(confirmationToken);
     }
 }
