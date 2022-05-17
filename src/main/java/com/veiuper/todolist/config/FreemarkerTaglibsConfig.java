5db5d1b2be50d15dad501b1a5f89da40526e48f7
veiuper <valeriyfinans@gmail.com>
2022-05-17 17:36:14 +0500

    Change FreemarkerTaglibsConfig.java

--- src/main/java/com/veiuper/todolist/config/FreemarkerTaglibsConfig.java
+++ src/main/java/com/veiuper/todolist/config/FreemarkerTaglibsConfig.java
@@ -1,13 +1,11 @@
 package com.veiuper.todolist.config;
 
 import com.veiuper.todolist.ClassPathTldsLoader;
-import lombok.AllArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@AllArgsConstructor
 public class FreemarkerTaglibsConfig {
     @Bean
     @ConditionalOnMissingBean(ClassPathTldsLoader.class)
