aa9a57d0fb495f44553aa647f78b35e3f59ca8f3
veiuper <valeriyfinans@gmail.com>
2022-05-17 12:26:42 +0500

    Change WebSecurityConfig.java

--- src/main/java/com/veiuper/todolist/config/WebSecurityConfig.java
+++ src/main/java/com/veiuper/todolist/config/WebSecurityConfig.java
@@ -3,22 +3,31 @@ package com.veiuper.todolist.config;
 import com.veiuper.todolist.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 @AllArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserService userService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
                 .authorizeRequests()
+                    //Доступ только для не зарегистрированных пользователей
+                    .antMatchers("/registration").not().fullyAuthenticated()
+                    //Доступ только для пользователей с ролью Администратор
+                    .antMatchers("/admin/**").hasRole("ADMIN")
+                    //Доступ разрешен всем пользователей
+                    .antMatchers("/", "/resources/**").permitAll()
                 .antMatchers("/sign-up/**", "/sign-in/**")
                 .permitAll()
                 .anyRequest()
@@ -27,6 +36,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/sign-in")
                 .permitAll();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Autowired
