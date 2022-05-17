a606d51fed6da1296aab5c4ab90016ecbdd09889
veiuper <valeriyfinans@gmail.com>
2022-05-17 19:46:35 +0500

    Comment out the content of the controllers

--- src/main/java/com/veiuper/todolist/controller/TaskController.java
+++ src/main/java/com/veiuper/todolist/controller/TaskController.java
@@ -1,49 +1,41 @@
 package com.veiuper.todolist.controller;
 
-import com.veiuper.todolist.model.TaskEntity;
-import com.veiuper.todolist.model.TasklistEntity;
-import com.veiuper.todolist.service.TaskService;
-import com.veiuper.todolist.service.TasklistService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Set;
 
 @Controller
 public class TaskController {
-    private final TaskService taskService;
-    private final TasklistService tasklistService;
-
-    public TaskController(TaskService taskService, TasklistService tasklistService) {
-        this.taskService = taskService;
-        this.tasklistService = tasklistService;
-    }
-
-    @GetMapping("/tasklist/{tasklistId}/tasks")
-    public String getAll(Model model, @PathVariable Long tasklistId) {
-        TasklistEntity tasklistEntity = tasklistService.getById(tasklistId);
-        Set<TaskEntity> taskEntityList = taskService.findByTasklistEntityIdOrderByExecutedAscIdAsc(tasklistId);
-        model.addAttribute("tasklist", tasklistEntity);
-        model.addAttribute("taskList", taskEntityList);
-        return "tasklist";
-    }
-
-    @PostMapping("/tasklist/{tasklistId}/add")
-    public String addTask(@ModelAttribute TaskEntity taskEntity, @PathVariable Long tasklistId) {
-        taskEntity.setTasklist(tasklistService.getById(tasklistId));
-        taskService.save(taskEntity);
-        return "redirect:/tasklist/{tasklistId}/tasks";
-    }
-    @RequestMapping("/tasklist/{tasklistId}/delete/{taskId}")
-    public String deleteTask(@PathVariable String tasklistId, @PathVariable Long taskId) {
-        taskService.delete(taskId);
-        return "redirect:/tasklist/{tasklistId}/tasks";
-    }
-
-    @RequestMapping("/tasklist/{tasklistId}/switchTaskStatus/{taskId}")
-    public String reversTaskStatus(@PathVariable Long tasklistId, @PathVariable Long taskId) throws Exception {
-        taskService.switchTaskStatus(taskId);
-        return "redirect:/tasklist/{tasklistId}/tasks";
-    }
+//    private final TaskService taskService;
+//    private final TasklistService tasklistService;
+//
+//    public TaskController(TaskService taskService, TasklistService tasklistService) {
+//        this.taskService = taskService;
+//        this.tasklistService = tasklistService;
+//    }
+//
+//    @GetMapping("/tasklist/{tasklistId}/tasks")
+//    public String getAll(Model model, @PathVariable Long tasklistId) {
+//        TasklistEntity tasklistEntity = tasklistService.getById(tasklistId);
+//        Set<TaskEntity> taskEntityList = taskService.findByTasklistEntityIdOrderByExecutedAscIdAsc(tasklistId);
+//        model.addAttribute("tasklist", tasklistEntity);
+//        model.addAttribute("taskList", taskEntityList);
+//        return "tasklist";
+//    }
+//
+//    @PostMapping("/tasklist/{tasklistId}/add")
+//    public String addTask(@ModelAttribute TaskEntity taskEntity, @PathVariable Long tasklistId) {
+//        taskEntity.setTasklist(tasklistService.getById(tasklistId));
+//        taskService.save(taskEntity);
+//        return "redirect:/tasklist/{tasklistId}/tasks";
+//    }
+//    @RequestMapping("/tasklist/{tasklistId}/delete/{taskId}")
+//    public String deleteTask(@PathVariable String tasklistId, @PathVariable Long taskId) {
+//        taskService.delete(taskId);
+//        return "redirect:/tasklist/{tasklistId}/tasks";
+//    }
+//
+//    @RequestMapping("/tasklist/{tasklistId}/switchTaskStatus/{taskId}")
+//    public String reversTaskStatus(@PathVariable Long tasklistId, @PathVariable Long taskId) throws Exception {
+//        taskService.switchTaskStatus(taskId);
+//        return "redirect:/tasklist/{tasklistId}/tasks";
+//    }
 }
--- src/main/java/com/veiuper/todolist/controller/UserController.java
+++ src/main/java/com/veiuper/todolist/controller/UserController.java
@@ -1,34 +1,29 @@
 package com.veiuper.todolist.controller;
 
-import com.veiuper.todolist.model.User;
-import com.veiuper.todolist.service.ConfirmationTokenService;
-import com.veiuper.todolist.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @AllArgsConstructor
 public class UserController {
-    private final UserService userService;
-    private final ConfirmationTokenService confirmationTokenService;
-
-    @GetMapping("/sign-in")
-    public String signIn() {
-        return "sign-in";
-    }
-
-    @GetMapping("/sign-up")
-    public String signUp() {
-        return "sign-up";
-    }
-
-    @PostMapping("/sign-up")
-    public String signUp(User user) {
-        userService.signUpUser(user);
-        return "redirect:/sign-in";
-    }
+//    private final UserService userService;
+//    private final ConfirmationTokenService confirmationTokenService;
+//
+//    @GetMapping("/sign-in")
+//    public String signIn() {
+//        return "sign-in";
+//    }
+//
+//    @GetMapping("/sign-up")
+//    public String signUp() {
+//        return "sign-up";
+//    }
+//
+//    @PostMapping("/sign-up")
+//    public String signUp(User user) {
+//        userService.signUpUser(user);
+//        return "redirect:/sign-in";
+//    }
 
 //    @GetMapping("/confirm")
 //    String confirmMail(@RequestParam("token") String token) {
