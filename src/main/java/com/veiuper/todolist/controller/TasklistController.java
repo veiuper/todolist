b9e7942d09cc888a472c4ae7fe88b3c7510fdecc
veiuper <valeriyfinans@gmail.com>
2022-05-19 15:44:09 +0500

    Change TasklistController.java

--- src/main/java/com/veiuper/todolist/controller/TasklistController.java
+++ src/main/java/com/veiuper/todolist/controller/TasklistController.java
@@ -1,31 +1,37 @@
 package com.veiuper.todolist.controller;
 
+import com.veiuper.todolist.model.TasklistEntity;
+import com.veiuper.todolist.service.TasklistService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class TasklistController {
-//    private final TasklistService tasklistService;
-//
-//    public TasklistController(TasklistService tasklistService) {
-//        this.tasklistService = tasklistService;
-//    }
-//
-//    @GetMapping("/")
-//    public String getAll(Model model) {
-//        List<TasklistEntity> tasklistEntityList = tasklistService.getAll();
-//        model.addAttribute("tasklistList", tasklistEntityList);
-//        return "index";
-//    }
-//
-//    @PostMapping("/add")
-//    public String addTasklist(@ModelAttribute TasklistEntity tasklistEntity) {
-//        tasklistService.save(tasklistEntity);
-//        return "redirect:/";
-//    }
-//
-//    @RequestMapping("/delete/{id}")
-//    public String deleteTasklist(@PathVariable Long id) {
-//        tasklistService.delete(id);
-//        return "redirect:/";
-//    }
+    private final TasklistService tasklistService;
+
+    public TasklistController(TasklistService tasklistService) {
+        this.tasklistService = tasklistService;
+    }
+
+    @GetMapping("/tasklists")
+    public String getAll(Model model) {
+        List<TasklistEntity> tasklistEntityList = tasklistService.getAll();
+        model.addAttribute("tasklistList", tasklistEntityList);
+        return "tasklists";
+    }
+
+    @PostMapping("/add")
+    public String addTasklist(@ModelAttribute TasklistEntity tasklistEntity) {
+        tasklistService.save(tasklistEntity);
+        return "redirect:/tasklists";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String deleteTasklist(@PathVariable Long id) {
+        tasklistService.delete(id);
+        return "redirect:/tasklists";
+    }
 }
