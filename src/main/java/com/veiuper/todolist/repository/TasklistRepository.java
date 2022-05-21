c7432d97579555cc43f38c8ad9c6534fe62d04e9
veiuper <valeriyfinans@gmail.com>
2022-05-22 01:22:10 +0500

    Change TasklistRepository.java
    Add findAllByUser(User user) method.

--- src/main/java/com/veiuper/todolist/repository/TasklistRepository.java
+++ src/main/java/com/veiuper/todolist/repository/TasklistRepository.java
@@ -1,9 +1,13 @@
 package com.veiuper.todolist.repository;
 
 import com.veiuper.todolist.model.TasklistEntity;
+import com.veiuper.todolist.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TasklistRepository extends JpaRepository<TasklistEntity, Long> {
+    List<TasklistEntity> findAllByUser(User user);
 }
